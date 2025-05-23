f9fa187e1d17ab200de7bd73c4fa667aca3d3f32
AveryPilcer <avery.pilcer@education.nsw.gov.au>
2025-05-23 15:04:28 +1000

    respawn shenigags

---
+++ Assets/GameController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Vector2 startPos;
+
+    private void ()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy")) ;
+        {
+            Die();
+        }
+    }
+}
