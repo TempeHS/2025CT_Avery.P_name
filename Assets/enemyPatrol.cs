cfae5914b12cd7d39baa64bc20a3c87457e34a9b
AveryPilcer <avery.pilcer@education.nsw.gov.au>
2025-06-23 10:32:04 +1000

    it works he moves

--- Assets/enemyPatrol.cs
+++ Assets/enemyPatrol.cs
@@ -32,6 +32,19 @@ public class enemyPatrol : MonoBehaviour
             rb.velocity = new Vector2(-speed, 0);
         }
 
-        if(Vector2.Distance(transform.position))
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
     }
 }
