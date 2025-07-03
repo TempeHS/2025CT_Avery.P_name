62fee686aaf031ccef9f2ba7d3b274e23d51a517
AveryPilcer <avery.pilcer@education.nsw.gov.au>
2025-07-03 15:04:15 +1000

    you can die now

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    
+    Vector2 startPos;
+    Rigidbody playerRb;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        StartCoroutine(Respawn(0.5f));
+    }
+
+    IEnumerator Respawn(float duration)
+    {
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(duration);
+        transform.position = startPos;
+        spriteRenderer.enabled = true;
+    }
 }
