6f331fdfa797126106ea167de9fcebc0ec4a3db8
AveryPilcer <avery.pilcer@education.nsw.gov.au>
2025-10-23 15:04:09 +1100

    attack animation is playing too much

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -15,6 +15,10 @@ public class PlayerAttack : MonoBehaviour
     public float attackMove;
     Animator anim;
     
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
     
         void Update()
         {
@@ -24,7 +28,8 @@ public class PlayerAttack : MonoBehaviour
                 if(Input.GetKey(KeyCode.Mouse0)){
                    // canAnim.SetTrigger("shake");
                     //playerAnim.SetTrigger("attack");
-                    anim.SetBool("attack", true);
+                    anim.SetBool("Attack", true);
+                    
                     Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
                    
                 for (int i = 0; i < enemiesToDamage.Length; i++)
